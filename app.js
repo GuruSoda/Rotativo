ed0bff4f9c261ae93f97d1b81fd75d52b5765071
Fabricio D. Rossi <frossi@bancocredicoop.coop>
2020-04-16 17:58:50 -0300

    Cambios para poner en produccion V2

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
+app.set('etag', false); // turn off
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
